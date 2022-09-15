34e7c613735c70294931dcdd30338c0a0f90285d
akifzahin <zahinakif25@gmail.com>
2022-09-15 22:09:50 +0600

    Updated Lab16 files

---
+++ src/com/AkifZahin/Lab_Exercises/Lab16/RunProgram.java
@@ -0,0 +1,10 @@
+package com.AkifZahin.Lab_Exercises.Lab16;
+//package com.AkifZahin.Lab16;
+//
+//public class RunProgram {
+//	public static void main(String[] args)
+//	{
+//		Welcome frame = new Welcome();
+//		frame.setVisible(true);
+//	}
+//}
